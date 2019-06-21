a6a2cb5789c39d75fa5126c476df36dac29bc6c9
Anindya Mozumdar <radmuzom@outlook.com>
2019-06-22 01:50:09 +0800

    Continued working

--- codes/module_distribution.R
+++ codes/module_distribution.R
@@ -1,3 +1,14 @@
+bin_numeric <- function(ds, nvar) {
+  nvar_v <- ds[, nvar]
+  nvar_v <- nvar_v[!is.na(nvar_v)]
+  nvar_q <- quantile(nvar_v, probs = seq(.1, 1, .1))
+  nvar_q <- unique(c(min(nvar_v), nvar_q))
+  nvar_g <- cut(nvar_v, breaks = nvar_q)
+  nvar_d <- data.frame(table(nvar_g))
+  names(nvar_d) <- c("Bin", "Frequency")
+  nvar_d
+}
+
 distribution_ui <- function(id) {
   ns <- NS(id)
   tagList(
@@ -30,7 +41,14 @@ distribution_server <- function(input, output, session, data_ds_vars) {
   }, priority = 10)
   
   observeEvent(input$varlist_distribution, {
+    validate(need(data_ds_vars, message = FALSE))
     req(input$varlist_distribution)
+    binned_vars[[input$varlist_distribution]] <<-
+      bin_numeric(data_ds_vars$data_ds(), input$varlist_distribution)
+    
+    output$binned_distribution <- renderTable({
+      binned_vars[[input$varlist_distribution]]
+    })
   })
   
   binned_vars
